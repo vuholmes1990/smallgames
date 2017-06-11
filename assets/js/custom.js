51ca559be84ce6bcb2b1f46203227244e6da05c9
VU HOLMES <vuholmes@gmail.com>
2017-06-11 12:40:45 -0700

    init

---
+++ assets/js/custom.js
@@ -0,0 +1,53 @@
+
+
+
+/*=============================================================
+    Authour URI: www.binarytheme.com
+    License: Commons Attribution 3.0
+
+    http://creativecommons.org/licenses/by/3.0/
+
+    100% To use For Personal And Commercial Use.
+    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+   
+    ========================================================  */
+
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+           
+            /*====================================
+              LOAD APPROPRIATE MENU BAR
+           ======================================*/
+            $(window).bind("load resize", function () {
+                if ($(this).width() < 768) {
+                    $('div.sidebar-collapse').addClass('collapse')
+					$('div.only-desktop').addClass('hidden')
+					$('div.only-mobile').removeClass('hidden')
+                } else {
+                    $('div.sidebar-collapse').removeClass('collapse')
+					$('div.only-desktop').removeClass('hidden')
+					$('div.only-mobile').addClass('hidden')
+                }
+            });
+
+          
+     
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing ///
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
